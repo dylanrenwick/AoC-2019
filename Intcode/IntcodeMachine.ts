c9824f464dff845e6b10cb8a0301e5a8efcf7768
dylanrenwick <skidsirongenesisdev@gmail.com>
2019-12-05 11:03:18 -0500

    Day 5

--- Intcode/IntcodeMachine.ts
+++ Intcode/IntcodeMachine.ts
@@ -1,19 +1,67 @@
+import * as readline from "readline";
+import { loopWhile } from "deasync";
+
 interface MachineStats {
     opsRun: number;
     startTime: Date | null;
     stopTime: Date | null;
 }
 
+interface Operation {
+    opFunc: (m: OperationMode[], i: number) => void;
+    parity: number
+}
+
 export default class IntcodeMachine {
     public debug: boolean = false;
 
     private memory: Array<number> = [];
-    private ops: { [i: number]: (i: number) => void } = {
-        1: i => this.memory[this.memory[i+3]] = this.memory[this.memory[i+1]] + this.memory[this.memory[i+2]],
-        2: i => this.memory[this.memory[i+3]] = this.memory[this.memory[i+1]] * this.memory[this.memory[i+2]],
-        3: i => this.memory[this.memory[i+3]] = this.memory[this.memory[i+1]] / this.memory[this.memory[i+2]],
-        4: i => this.ip = this.memory[this.memory[i+1]],
-        99: i => this.running = false
+    private ops: { [i: number]: Operation } = {
+        // add(a, b, dest)
+        1: {
+            opFunc: (m, i) => this.setParam(i+3, this.getParam(m[0]||0, i+1) + this.getParam(m[1]||0, i+2)),
+            parity: 4
+        },
+        // mul(a, b, dest)
+        2: {
+            opFunc: (m, i) => this.setParam(i+3, this.getParam(m[0]||0, i+1) * this.getParam(m[1]||0, i+2)),
+            parity: 4
+        },
+        // input(dest)
+        3: {
+            opFunc: (m, i) => this.setParam(i+1, this.readInput()),
+            parity: 2
+        },
+        // output(a)
+        4: {
+            opFunc: (m, i) => console.log(this.getParam(m[0]||0, i+1)),
+            parity: 2
+        },
+        // jmpnz(a, b)
+        5: {
+            opFunc: (m, i) => this.ip = (this.getParam(m[0]||0, i+1)) ? this.getParam(m[1]||0, i+2) - 3 : this.ip,
+            parity: 3
+        },
+        // jmpz(a, b)
+        6: {
+            opFunc: (m, i) => this.ip = (!this.getParam(m[0]||0, i+1)) ? this.getParam(m[1]||0, i+2) - 3 : this.ip,
+            parity: 3
+        },
+        // le(a, b, dest)
+        7: {
+            opFunc: (m, i) => this.setParam(i+3, (this.getParam(m[0]||0, i+1) < this.getParam(m[1]||0, i+2)) ? 1 : 0),
+            parity: 4
+        },
+        // eq(a, b, dest)
+        8: {
+            opFunc: (m, i) => this.setParam(i+3, (this.getParam(m[0]||0, i+1) === this.getParam(m[1]||0, i+2)) ? 1 : 0),
+            parity: 4
+        },
+        // exit()
+        99: {
+            opFunc: (m, i) => this.running = false,
+            parity: 1
+        }
     };
 
     private running: boolean = false;
@@ -39,8 +87,8 @@ export default class IntcodeMachine {
         this.stats.startTime = new Date();
 
         while (this.running) {
-            this.executeOp(this.ip);
-            this.ip += 4;
+            let ipAdv = this.executeOp(this.ip);
+            this.ip += ipAdv;
             if (this.ip >= this.memory.length) {
                 this.stats.stopTime = new Date();
                 this.running = false;
@@ -80,18 +128,31 @@ export default class IntcodeMachine {
         };
     }
 
-    private executeOp(position) {
+    private executeOp(position): number {
         this.stats.opsRun++;
-        if (this.debug) console.log(`Running opcode at position ${position}\nMemory dump:`);
+        if (this.debug) { console.log(`Running opcode at position ${position}\nMemory dump:`); this.memoryDump(); }
         if (position < 0 || position >= this.memory.length) {
             this.error("Memory access violation: " + position + " is outside the bounds of valid memory");
-            return;
+            return -1;
         }
-        if (this.ops[this.memory[position]] === undefined) {
-            this.error("Invalid opcode: " + this.memory[position] + " at position " + position);
-            return;
+
+        let instructionCode = this.memory[position].toString().split("");
+        let opCode = parseInt(instructionCode.slice(-2).join(""));
+        let modes = instructionCode.slice(0, -2).reverse().map(d => parseInt(d) as OperationMode);
+
+        let op: Operation = this.ops[opCode];
+
+        if (op === undefined) {
+            this.error("Invalid opcode: " + opCode + " at position " + position);
+            return -1;
         }
-        this.ops[this.memory[position]](position);
+
+        if (modes.length < op.parity) modes = modes.concat([...new Array(op.parity - modes.length)].map(_ => 0));
+
+        if (this.debug) console.log(`${instructionCode.join("")}: OpCode is ${opCode} and modes are ${modes.join(",")}`);
+
+        op.opFunc(modes, position);
+        return op.parity;
     }
 
     private parseState(val) {
@@ -101,8 +162,9 @@ export default class IntcodeMachine {
 
     private memoryDump() {
         for (let i = 0; i < this.memory.length; i += 4) {
-            console.log(`${i.toString()}: ${this.memory.slice(i, i + 4).join(", ")}`);
+            console.log(`0x${i.toString(16).padStart(2, '0')}: ${this.memory.slice(i, i + 4).join(", ")}`);
         }
+        console.log(`IP: ${this.ip}`);
     }
 
     private error(message) {
@@ -111,4 +173,40 @@ export default class IntcodeMachine {
         console.log(`Error: ${message}\n\nMemory dump:`);
         this.memoryDump();
     }
+
+    private getParam(mode: OperationMode, index: number): number {
+        if (this.debug) console.log(`Fetching param at index ${index} with mode ${mode}`);
+        switch (mode) {
+            case OperationMode.PositionMode:
+                return this.memory[this.memory[index]];
+            case OperationMode.ImmediateMode:
+                return this.memory[index];
+            default:
+                return -1;
+        }
+    }
+
+    private setParam(index: number, value: number): void {
+        if (this.debug) console.log(`Setting param at index ${index} to ${value}`);
+        this.memory[this.memory[index]] = value;
+    }
+
+    private readInput(): number {
+        const rl = readline.createInterface(process.stdin);
+        var done = false;
+        var data: string;
+        rl.question("", a => {
+            data = a;
+            done = true;
+            rl.close();
+        });
+        loopWhile(() => !done);
+
+        return parseInt(data.trim());
+    }
+}
+
+enum OperationMode {
+    PositionMode,
+    ImmediateMode
 }
